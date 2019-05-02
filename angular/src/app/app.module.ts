2d81f300502be65ee55805bdcbead662f9cab9da
hackleman <hackleman.jason@gmail.com>
2019-05-02 15:15:47 -0500

    structural changes to app modules.  Folder structure reflects type of page

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -1,3 +1,4 @@
+// Angular / browser modules
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
@@ -5,59 +6,56 @@ import { HttpClientModule } from '@angular/common/http';
 import { RouterModule, Routes } from '@angular/router';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { Ng5SliderModule } from 'ng5-slider';
+import { ChartsModule } from 'ng2-charts';
 
+// Base App Components
 import { AppComponent } from './app.component';
-import { MapsComponent } from './maps/maps.component';
 import { NavbarComponent } from './navbar/navbar.component';
-import { LoginComponent } from './login/login.component';
-import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
 import { HomeComponent } from './home/home.component';
 import { TestComponent } from './test/test.component';
-import { MapresultComponent } from './mapresult/mapresult.component';
-import { NetflowComponent } from './netflow/netflow.component';
-import { CostComponent } from './cost/cost.component';
-import { TimegraphComponent } from './timegraph/timegraph.component';
 
+// Map related components
+import { CostComponent } from './map/cost/cost.component';
+import { MapsComponent } from './map/maps/maps.component';
+import { MapsindexComponent } from './map/mapsindex/mapsindex.component';
+import { NetflowComponent } from './map/netflow/netflow.component';
+import { WeathermapComponent } from './map/weathermap/weathermap.component';
+import { WeathertypemapComponent } from './map/weathertypemap/weathertypemap.component';
+import { WindmapComponent } from './map/windmap/windmap.component';
+
+// Chart related components
+import { ChartsindexComponent } from './charts/chartsindex/chartsindex.component';
+import { NetflowchartComponent } from './charts/netflowchart/netflowchart.component';
+import { TestchartComponent } from './charts/testchart/testchart.component';
+import { WeatherchartComponent } from './charts/weatherchart/weatherchart.component';
+import { WeathercostComponent } from './charts/weathercost/weathercost.component';
+import { WeathertripavgComponent } from './charts/weathertripavg/weathertripavg.component';
+
+// App services
 import { ValidateService } from './services/validate.service';
 import { DataService } from './services/data.service';
 import { MessageService } from './services/message.service';
 
-import { MapsindexComponent } from './mapsindex/mapsindex.component';
-import { ChartsindexComponent } from './chartsindex/chartsindex.component';
-import { TestchartComponent } from './charts/testchart/testchart.component';
-import { ChartsModule } from 'ng2-charts';
-import { WeatherchartComponent } from './charts/weatherchart/weatherchart.component';
-import { WeathercostComponent } from './charts/weathercost/weathercost.component';
-import { WeathertripavgComponent } from './charts/weathertripavg/weathertripavg.component';
-import { WindmapComponent } from './windmap/windmap.component';
-import { WeathertypemapComponent } from './weathertypemap/weathertypemap.component';
-import { WeathermapComponent } from './weathermap/weathermap.component';
-import { NetflowchartComponent } from './charts/netflowchart/netflowchart.component';
-
-
-
-
 const appRoutes: Routes = [
-  {path:'maps', component: MapsComponent},
-  {path:'login', component: LoginComponent},
-  {path:'register', component: RegisterComponent},
-  {path:'', component: HomeComponent},
-  {path:'test', component: TestComponent},
-  {path:'mapresults', component: MapresultComponent},
-  {path:'netflow', component: NetflowComponent},
-  {path: 'cost', component: CostComponent},
-  {path: 'timegraph', component: TimegraphComponent},
-  {path: 'mapsindex', component: MapsindexComponent},
-  {path: 'chartsindex', component: ChartsindexComponent},
-  {path: 'testchart', component: TestchartComponent},
-  {path: 'weatherchart', component: WeatherchartComponent},
-  {path: 'weathercost', component: WeathercostComponent},
-  {path: 'weathermap', component: WeathermapComponent},
-  {path: 'windmap', component: WindmapComponent},
-  {path: 'weathertype', component: WeathertypemapComponent},
-  {path: 'weathertripavg', component: WeathertripavgComponent},
-  {path: 'netflowchart', component: NetflowchartComponent}
-
+  {path: 'login', component: LoginComponent},
+  {path: 'register', component: RegisterComponent},
+  {path: '', component: HomeComponent},
+  {path: 'test', component: TestComponent},
+  {path: 'maps', component: MapsindexComponent},
+  {path: 'maps/cost', component: CostComponent},
+  {path: 'maps/map', component: MapsComponent},
+  {path: 'maps/netflow', component: NetflowComponent},
+  {path: 'maps/weathermap', component: WeathermapComponent},
+  {path: 'maps/weathertype', component: WeathertypemapComponent},
+  {path: 'maps/windmap', component: WindmapComponent},
+  {path: 'charts', component: ChartsindexComponent},
+  {path: 'charts/netflowchart', component: NetflowchartComponent},
+  {path: 'charts/testchart', component: TestchartComponent},
+  {path: 'charts/weatherchart', component: WeatherchartComponent},
+  {path: 'charts/weathercost', component: WeathercostComponent},
+  {path: 'charts/weathertripavg', component: WeathertripavgComponent}
 ]
 
 @NgModule({
@@ -69,10 +67,8 @@ const appRoutes: Routes = [
     RegisterComponent,
     HomeComponent,
     TestComponent,
-    MapresultComponent,
     NetflowComponent,
     CostComponent,
-    TimegraphComponent,
     MapsindexComponent,
     ChartsindexComponent,
     TestchartComponent,
---
+++ angular/src/app/map/mapsindex/mapsindex.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { ViewChild } from '@angular/core';
+import { ValidateService } from '../../services/validate.service';
+import { DataService } from '../../services/data.service';
+import { MessageService } from '../../services/message.service';
+import { Router } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+
+@Component({
+  selector: 'app-mapsindex',
+  templateUrl: './mapsindex.component.html',
+  styleUrls: ['./mapsindex.component.css']
+})
+export class MapsindexComponent implements OnInit {
+
+  constructor(
+    private validateService: ValidateService,
+    private dataService: DataService,
+    private messageService: MessageService,
+    private router: Router
+  ) { }
+
+  ngOnInit() {
+  }
+
+}
